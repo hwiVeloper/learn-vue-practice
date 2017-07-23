d879679c363545404799faa55510052b17e16da6
hwiVeloper <zziller03@gmail.com>
2017-07-23 17:27:35 +0900

    컴포넌트

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks">
+                {{ task.description }}
+            </task>
+        </div>`,
+
+    data() {
+        return {
+            tasks: [
+                { description: 'Go to the store', completed: true },
+                { description: 'Finish Screencast', completed: false },
+                { description: 'Make donation', completed: false },
+                { description: 'Clear inbox', completed: false },
+                { description: 'Make dinner', completed: true },
+                { description: 'Clean room', completed: false },
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>',
-
 });
 
 new Vue({
