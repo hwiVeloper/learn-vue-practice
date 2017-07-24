37562b2229eca99679b49730b3ae7547d2282a04
hwiVeloper <zziller03@gmail.com>
2017-07-24 15:13:23 +0900

    실시간 값 대체

---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+      <h2>The Count : {{ count }}</h2>
+
+      <p>
+          <button type="button" @click="count += 2">Increment</button>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { count: 0 };
+    }
+}
+</script>
+
+<style lang="css">
+</style>
