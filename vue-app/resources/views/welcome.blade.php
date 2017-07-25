80f0c42f4730d2da675e68c9d6aa882d27c440e9
hwiVeloper <zziller03@gmail.com>
2017-07-26 01:01:06 +0900

    shared state

--- vue-app/resources/views/welcome.blade.php
+++ vue-app/resources/views/welcome.blade.php
@@ -6,17 +6,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link rel="stylesheet" href="/css/app.css">
+        {{-- <link rel="stylesheet" href="/css/app.css"> --}}
     </head>
     <body>
         <div class="" id="app">
-            <notification></notification>
+            {{-- <notification></notification> --}}
+        </div>
+
+        <div id="one">
+            <h1>
+                @{{ shared.user.name }}
+            </h1>
+        </div>
+
+        <div id="two">
+            <h1>
+                @{{ shared.user.name }}
+            </h1>
         </div>
 
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
         <script type="text/javascript" src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
 
-        <script src="/js/app.js"></script>
+        <script src="/js/app-state.js"></script>
     </body>
 </html>
