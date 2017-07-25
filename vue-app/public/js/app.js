ef94d952c5d46e37a3e6072987c2f0158e8ad42d
hwiVeloper <zziller03@gmail.com>
2017-07-25 14:12:12 +0900

    객체지향 폼 일부 수정

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -30,13 +30,13 @@ class Errors {
             return;
         }
 
-        this.error = {};
+        this.errors = {};
     }
 }
 
 class Form {
     constructor(data) {
-        this.orginalData = data;
+        this.originalData = data;
 
         for (let field in data) {
             this[field] = data[field];
@@ -45,60 +45,39 @@ class Form {
         this.errors = new Errors();
     }
 
-    data() {
-        let data = {};
-
-        for (let property in this.originalData) {
-            data[property] = this[property];
-        }
-
-        return data;
-    }
-
     reset() {
         for (let field in this.originalData) {
             this[field] = '';
         }
-
-        this.errors.clear();
     }
 
-    post(url) {
-        return this.submit('post', url)
-    }
+    data() {
+        let data = Object.assign({}, this);
 
-    delete(url) {
-        return this.submit('delete', url);
+        delete data.originalData;
+        delete data.errors;
+
+        return data;
     }
 
     submit(requestType, url) {
-        return new Promise((resolve, reject) => {
-            axios[requestType](url, this.data())
-                .then(response => {
-                    this.onSuccess(response.data);
-
-                    resolve(response.data);
-                })
-                .catch(error => {
-                    this.onFail(error.response.data);
-
-                    reject(error.response.data);
-                });
-        });
+        axios[requestType](url, this.data())
+            .then(this.onSuccess.bind(this))
+            .catch(this.onFail.bind(this));
     }
 
-    onSuccess(data) {
-        alert(data.message);
+    onSuccess(response) {
+        alert(response.data.message);
 
         this.errors.clear();
-
         this.reset();
     }
 
-    onFail(errors) {
-        this.errors.record(errors);
+    onFail(error) {
+        this.errors.record(error.response.data);
     }
 }
+
 
 new Vue({
     el: '#app',
@@ -112,10 +91,7 @@ new Vue({
 
     methods: {
         onSubmit() {
-            // this.form.submit('post', '/projects')
-            //     .then(data => console.log(data))
-            //     .catch(errors => console.log(errors));
-            this.form.delete('/projects');
+            this.form.submit('post', '/projects');
         }
     }
 });
